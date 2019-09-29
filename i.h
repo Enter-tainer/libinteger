003a2170037726b9208b847503995093340b06c5
Banson <Banson Xu>
2019-09-29 22:50:12 +0800

    (unsigned)long long and string support for operator=()

--- i.h
+++ i.h
@@ -8,106 +8,98 @@
 #include "basic_uint.h"
 
 namespace mgt {
-  template<size_t l = 1024>
-  class i : public basic_uint<l> {
-  public:
-    i() {
-      this->data = 0;
+template <size_t l = 1024> class i : public basic_uint<l> {
+public:
+  i() { this->data = 0; }
+
+  i(const i<l> &s) { this->data = s.data; }
+
+  i(const basic_uint<l> &num) : basic_uint<l>(num) {}
+
+  explicit i(long long s) {
+    if (s >= 0)
+      this->data = std::bitset<l>(s);
+    else {
+      this->data = std::bitset<l>(-s);
+      (*this) = (~*this) + basic_uint<l>(1);
     }
+  }
 
-    i(const i<l> &s) { this->data = s.data; }
+  explicit i(const std::string str) { this->from_string(str); }
 
-    i(const basic_uint<l> &num) : basic_uint<l>(num) {}
+  explicit i(std::bitset<l> s) { this->data = s; }
 
-    explicit i(long long s) {
-      if (s >= 0)
-        this->data = std::bitset<l>(s);
-      else {
-        this->data = std::bitset<l>(-s);
-        (*this) = (~*this) + basic_uint<l>(1);
-      }
+  void from_string(const std::string &str) {
+    if (str[0] != '-')
+      this->data = this->basic_from_string(str);
+    else {
+      std::string tmp;
+      for (size_t i = 1; i < str.length(); ++i)
+        tmp.push_back(str[i]);
+      this->data = this->basic_from_string(str);
+      (*this) = ~(*this);
+      (*this) = (*this) + i<l>(1);
     }
+  }
 
-    explicit i(const std::string str) {
-      this->from_string(str);
-    }
+  [[nodiscard]] virtual std::string to_string() const {
+    if (this->data[l - 1])
+      return "-" + this->basic_to_string((~(*this)) + basic_uint<l>(1));
+    else
+      return this->basic_to_string(*this);
+  }
 
-    explicit i(std::bitset<l> s) {
-      this->data = s;
-    }
+  i<l> &operator=(const long long s) {
+    *this = i<l>(s);
+    return *this;
+  }
 
-    void from_string(const std::string &str) {
-      if (str[0] != '-')
-        this->data = this->basic_from_string(str);
-      else {
-        std::string tmp;
-        for (size_t i = 1; i < str.length(); ++i)
-          tmp.push_back(str[i]);
-        this->data = this->basic_from_string(str);
-        (*this) = ~(*this);
-        (*this) = (*this) + i < l > (1);
-      }
-    }
+  i<l> &operator=(const std::string str) {
+    *this = i<l>(str);
+    return *this;
+  }
 
-    [[nodiscard]] virtual std::string to_string() const {
-      if (this->data[l - 1])
-        return "-" + this->basic_to_string((~(*this)) + basic_uint<l>(1));
-      else
-        return this->basic_to_string(*this);
-    }
+  friend i<l> operator+(const i<l> &a, const i<l> &b) {
+    return i<l>((basic_uint<l>)a + (basic_uint<l>)b);
+  }
 
-    friend i<l> operator+(const i<l> &a, const i<l> &b) {
-      return i<l>((basic_uint<l>) a + (basic_uint<l>) b);
-    }
+  friend i<l> operator-(const i<l> &a, const i<l> &b) {
+    return i<l>((basic_uint<l>)a - (basic_uint<l>)b);
+  }
 
-    friend i<l> operator-(const i<l> &a, const i<l> &b) {
-      return i<l>((basic_uint<l>) a - (basic_uint<l>) b);
-    }
+  friend i<l> operator*(const i<l> &a, const i<l> &b) {
+    return i<l>((basic_uint<l>)a * (basic_uint<l>)b);
+  }
 
-    friend i<l> operator*(const i<l> &a, const i<l> &b) {
-      return i<l>((basic_uint<l>) a * (basic_uint<l>) b);
-    }
+  friend i<l> operator/(const i<l> &a, const i<l> &b) {
+    return i<l>((basic_uint<l>)a / (basic_uint<l>)b);
+  }
 
-    friend i<l> operator/(const i<l> &a, const i<l> &b) {
-      return i<l>((basic_uint<l>) a / (basic_uint<l>) b);
-    }
+  friend std::ostream &operator<<(std::ostream &out, const i<l> &d) {
+    return out << d.to_string();
+  }
 
-    friend std::ostream &operator<<(std::ostream &out, const i<l> &d) {
-      return out << d.to_string();
-    }
+  friend bool operator<(const i<l> &a, const i<l> &b) { return is_neg(a - b); }
 
-    friend bool operator<(const i<l> &a, const i<l> &b) {
-      return is_neg(a - b);
-    }
+  friend bool operator<=(const i<l> &a, const i<l> &b) {
+    return a < b || a == b;
+  }
 
-    friend bool operator<=(const i<l> &a, const i<l> &b) {
-      return a < b || a == b;
-    }
+  friend bool operator>(const i<l> &a, const i<l> &b) { return !(a <= b); }
 
-    friend bool operator>(const i<l> &a, const i<l> &b) {
-      return !(a <= b);
-    }
+  friend bool operator>=(const i<l> &a, const i<l> &b) { return !(a < b); }
 
-    friend bool operator>=(const i<l> &a, const i<l> &b) {
-      return !(a < b);
-    }
+  friend bool operator!=(const i<l> &a, const i<l> &b) { return !(a == b); }
 
-    friend bool operator!=(const i<l> &a, const i<l> &b) {
-      return !(a == b);
-    }
+  friend std::istream &operator>>(std::istream &in, i<l> &d) {
+    std::string res;
+    in >> res;
+    d.from_string(res);
+    return in;
+  }
 
-    friend std::istream &operator>>(std::istream &in, i<l> &d) {
-      std::string res;
-      in >> res;
-      d.from_string(res);
-      return in;
-    }
+  friend bool is_neg(const i<l> &num) { return num.data[l - 1]; }
+};
 
-    friend bool is_neg(const i<l> &num) {
-      return num.data[l - 1];
-    }
-  };
-
-
-}
-#endif //INTEGER_I_H
+} // namespace mgt
+#endif // INTEGER_I_H
--- u.h
+++ u.h
@@ -8,28 +8,32 @@
 #include "basic_uint.h"
 
 namespace mgt {
-  template<size_t l = 1024>
-  class u : public basic_uint<l> {
-  public:
-    u() {
-      this->data = 0;
+template <size_t l = 1024> class u : public basic_uint<l> {
+public:
+  u() { this->data = 0; }
+
+  u(const u<l> &s) { this->data = s.data; }
+
+  explicit u(unsigned long long s) { this->data = std::bitset<l>(s); }
+
+  explicit u(std::bitset<l> s) { this->data = s; }
+
+  explicit u(std::string str) { this->from_string(str); }
+
+  u<l> &operator=(const unsigned long long s) {
+    *this = u<l>(s);
+    return *this;
+  }
+
+  u<l> &operator=(const std::string str) {
+    if (str[0] == '-') {
+      abort();
     }
+    *this = u<l>(str);
+    return *this;
+  }
+};
 
-    u(const u<l> &s) { this->data = s.data; }
+} // namespace mgt
 
-    explicit u(unsigned long long s) {
-      this->data = std::bitset<l>(s);
-    }
-
-    explicit u(std::bitset<l> s) {
-      this->data = s;
-    }
-
-    explicit u(std::string str) {
-      this->from_string(str);
-    }
-  };
-
-}
-
-#endif //INTEGER_U_H
+#endif // INTEGER_U_H
